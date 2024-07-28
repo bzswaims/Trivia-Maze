0df96d7aef58dae59e47330d057f0f45ace50ab3
bzswaims <zswaims@gmail.com>
2024-07-27 20:53:29 -0700

    database should be populated with test questions now, ignore the code in question factory, ill be working on it tomorrow

--- src/view/QuestionFactory.java
+++ src/view/QuestionFactory.java
@@ -18,105 +18,79 @@ public class QuestionFactory {
         //in the next line
         query = "null";
 
-        createDataBase();
         populateDataBase();
     }
 
-    public void createDataBase() {
-        try {
-            ds = new SQLiteDataSource();
-            ds.setUrl("jbdc:sqlite:questions.sqlite");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-
-        //Here I made a SUPER QUERY as I call it, it just builds 3 tables I hope. It has not been tested yet,
-        //that is for later tonight or tomorrow
-        query = "CREATE TABLE IF NOT EXISTS multiquestions ( " +
-                "QUESTION TEXT NOT NULL, " +
-                "CORRECTANSWER TEXT NOT NULL " +
-                "INCORRECTANSWERONE TEXT NOT NULL " +
-                "INCORRECTANSWERTWO TEXT NOT NULL " +
-                "INCORRECTANSWERTHREE TEXT NOT NULL ) " +
-                "CREATE TABLE IF NOT EXISTS truefalsequestions ( " +
-                "QUESTION TEXT NOT NULL, " +
-                "CORRECTANSWER TEXT NOT NULL )" +
-                "CREATE TABLE IF NOT EXISTS shortquestions ( " +
-                "QUESTION TEXT NOT NULL, " +
-                "CORRECTANSWER TEXT NOT NULL )";
-        try ( Connection conn = ds.getConnection();
-              Statement stmt = conn.createStatement(); ) {
-            int rv = stmt.executeUpdate( query );
-            System.out.println( "executeUpdate() returned " + rv );
-        } catch ( SQLException e ) {
-            e.printStackTrace();
-            System.exit( 0 );
-        }
-    }
-
     public void populateDataBase(){
-        //going to be 30 questions
-        //im looking at 10 multiple choice
-        //10 t/f
-        //10 short answer
-
-        //The plan with these are, since we add all into 3 different tables, we can create the questions,
-        //with the multiple choice questions, we can jumble the answers like they do on canvas when we take our quizes
-        //Next up is the true false, it just stores the correct of the two (no reason to store the incorrect value)
-        //And short answer just stores the correct answer to do a comparison to when the user inputs something.
+        //database structure is as follows:
+        //Questions table, has columns Question Text and Question Type
+        //question type 1: multiple choice
+        //question type 2: true/false
+        //question type 3: short answer
         try ( Connection conn = ds.getConnection();
               Statement stmt = conn.createStatement(); ) {
             //THESE ARE THE MULTIPLE CHOICE QUESTIONS\\
 
             //TODO: adjust to a real question
-            query = "INSERT INTO multiquestions ( QUESTION, CORRECTANSWER, " +
-                    "INCORRECTANSWERONE, INCORRECTANSWERTWO, " +
-                    "INCORRECTANSWERTHREE ) VALUES ( 'Test Multiple Choice " +
-                    "Question One?', 'Correct', 'Incorrect', 'Incorrect', 'Incorrect' )";
+            query = "INSERT INTO Questions ( QuestionText, QuestionType, ) VALUES ( 'Test Multiple Choice " +
+                    "Question One?', '1' )";
 
             int rv = stmt.executeUpdate( query );
-
-            //TODO: adjust to a real question
-            query = "INSERT INTO multiquestions ( QUESTION, CORRECTANSWER, " +
-                    "INCORRECTANSWERONE, INCORRECTANSWERTWO, " +
-                    "INCORRECTANSWERTHREE ) VALUES ( 'Test Multiple Choice " +
-                    "Question Two?', 'Correct', 'Incorrect', 'Incorrect', 'Incorrect' )";
-
-            rv = stmt.executeUpdate( query );
 
             //THESE ARE THE TRUE FALSE QUESTIONS\\
 
             //TODO: adjust to a real question
-            query = "INSERT INTO truefalsequestions ( QUESTION, CORRECTANSWER, " +
-                    "INCORRECTANSWERONE, INCORRECTANSWERTWO, " +
-                    "INCORRECTANSWERTHREE ) VALUES ( 'Test Multiple Choice " +
-                    "Question One?', 'False' )";
-
-            rv = stmt.executeUpdate( query );
-
-            //TODO: adjust to a real question
-            query = "INSERT INTO truefalsequestions ( QUESTION, CORRECTANSWER, " +
-                    "INCORRECTANSWERONE ) VALUES ( 'Test Multiple Choice " +
-                    "Question Two?', 'True' )";
+            query = "INSERT INTO Questions ( QuestionText, QuestionType ) VALUES ( 'Test True False " +
+                    "Question One?', '2' )";
 
             rv = stmt.executeUpdate( query );
 
             //THESE ARE THE SHORT ANSWER QUESTIONS\\
 
             //TODO: adjust to a real question
-            query = "INSERT INTO shortquestions ( QUESTION, CORRECTANSWER, " +
-                    "INCORRECTANSWERONE, INCORRECTANSWERTWO, " +
-                    "INCORRECTANSWERTHREE ) VALUES ( 'Test Multiple Choice " +
-                    "Question One?', 'Correct' )";
+            query = "INSERT INTO Questions ( QuestionText, QuestionType ) VALUES ( 'Test Short Answer " +
+                    "Question One?', '3' )";
 
             rv = stmt.executeUpdate( query );
 
-            //TODO: adjust to a real question
-            query = "INSERT INTO shortquestions ( QUESTION, CORRECTANSWER, " +
-                    "INCORRECTANSWERONE, INCORRECTANSWERTWO, " +
-                    "INCORRECTANSWERTHREE ) VALUES ( 'Test Multiple Choice " +
-                    "Question Two?', 'Correct' )";
+            //POPULATAING ANSWERS\\
+
+            //TODO: adjust to a real answer
+            query = "INSERT INTO Answers ( QuestionID, AnswerText, " +
+                    "IsCorrect ) VALUES ( '0', 'Test Multiple Choice " +
+                    "Answer?', 'True' )";
+
+            rv = stmt.executeUpdate( query );
+
+            //TODO: adjust to a real answer
+            query = "INSERT INTO Answers ( QuestionID, AnswerText, " +
+                    "IsCorrect ) VALUES ( '0', 'Test Multiple Choice " +
+                    "Answer True?', 'True' )";
+
+            rv = stmt.executeUpdate( query );
+
+            //TODO: adjust to a real answer
+            query = "INSERT INTO Answers ( QuestionID, AnswerText, " +
+                    "IsCorrect ) VALUES ( '0', 'Test Multiple Choice " +
+                    "Answer False?', 'False' )";
+
+            rv = stmt.executeUpdate( query );
+
+            //TODO: adjust to a real answer
+            query = "INSERT INTO Answers ( QuestionID, AnswerText, " +
+                    "IsCorrect ) VALUES ( '1', 'True', 'False' )";
+
+            rv = stmt.executeUpdate( query );
+
+            //TODO: adjust to a real answer
+            query = "INSERT INTO Answers ( QuestionID, AnswerText, " +
+                    "IsCorrect ) VALUES ( '1', 'False', 'True' )";
+
+            rv = stmt.executeUpdate( query );
+
+            //TODO: adjust to a real answer
+            query = "INSERT INTO Answers ( QuestionID, AnswerText, " +
+                    "IsCorrect ) VALUES ( '2', 'Answer', 'True' )";
 
             rv = stmt.executeUpdate( query );
         } catch ( SQLException e ) {
