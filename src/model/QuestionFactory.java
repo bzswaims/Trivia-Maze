43ab8ba835469d019cfa5d3bb5b7090d8ffc3d34
bzswaims <zswaims@gmail.com>
2024-08-04 15:46:04 -0700

    Finished up the QuestionFactory mostly, needs some minor tweaking, but it now creates questions and can spit out a random question of a type of the clients choice.

--- src/model/QuestionFactory.java
+++ src/model/QuestionFactory.java
@@ -14,11 +14,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Random;
 
 //TODO: make it properly, mostly toying with database since I have never worked with one before.
 
 /**
- * Gathers questions from database.
+ * Gathers questions from database. All question types should be kept together.
  *
  * @author Zane Swaims (bzswaims@uw.edu)
  * @version 0.1
@@ -36,6 +37,26 @@ public class QuestionFactory {
     ArrayList<AbstractQuestion> myQuestions;
 
     /**
+     * random number generator
+     */
+    private final Random myRand;
+
+    /**
+     * Amount of multiple choice questions
+     */
+    private int myMultiAmt;
+
+    /**
+     * Amount of true false choice questions
+     */
+    private int myTFAmt;
+
+    /**
+     * Amount of short choice questions
+     */
+    private int myShortAmt;
+
+    /**
      * Constructor.
      */
     public QuestionFactory() {
@@ -48,9 +69,13 @@ public class QuestionFactory {
             System.exit(0);
         }
 
-        //call build question
+        myMultiAmt = 0;
+        myTFAmt = 0;
+        myShortAmt = 0;
 
         myQuestions = new ArrayList<>();
+
+        myRand = new Random();
 
         buildQuestion();
     }
@@ -86,12 +111,15 @@ public class QuestionFactory {
                     case 1:
                         MultiQuestion questm = new MultiQuestion(questionText, "", questionID, questionType);
                         myQuestions.add(questm);
+                        myMultiAmt++;
                     case 2:
                         TrueFalseQuestion questtf = new TrueFalseQuestion(questionText, "", questionID, questionType);
                         myQuestions.add(questtf);
+                        myTFAmt++;
                     case 3:
                         ShortQuestion quests = new ShortQuestion(questionText, "", questionID, questionType);
                         myQuestions.add(quests);
+                        myShortAmt++;
                 }
             }
 
@@ -128,8 +156,53 @@ public class QuestionFactory {
         }
     }
 
-    public Question getQuestion(int Type) {
-        return null;
+    /**
+     * This will return a question, fully built, from the database of the type
+     * asked for
+     * @param theType
+     * @return
+     */
+    public Question getQuestion(final int theType) {
+        //1 is multi, 2 is tf, 3 is short, im thinking 4 is just any.
+
+        //my plan:
+        //Step one: Generate random number from 0 to 29.
+        //Step two: Mod random number by 3 (or just random number generate up to 3 since we have 3 question types)
+        //Step three: step through the list of questions start at 0, and go up by the random number until we find the
+        //            question type we need
+        //Step four: if we reach the end or past the end, off set by 1 and restart the process.
+        //Step five: if we find it, return the question, if we do not find it (IE the mod number is less than the offset)
+        //           return null.
+
+        //new plan, I have added amounts of each question in the array list, they are stored in the following order:
+        //multi, then tf, then short answer.
+
+        //TODO: lastly, I need a way to flag a question as having been pulled from the list once.
+        // So we do not reuse questions, I have steps to figure out one that has not been used yet, but need a flag.
+        // I will likely add a flag to the question, boolean ifUsed and check that one, set them all as false
+        // then if all have been used, I do not know what to do from there
+
+        int randomQuestion;
+
+        AbstractQuestion tempQuestion;
+
+        switch(theType) {
+            case 1:
+                randomQuestion = myRand.nextInt(myMultiAmt);
+                tempQuestion = myQuestions.get(randomQuestion);
+            case 2:
+                randomQuestion = myRand.nextInt(myTFAmt);
+                tempQuestion = myQuestions.get(randomQuestion + myMultiAmt);
+            case 3:
+                randomQuestion = myRand.nextInt(myShortAmt);
+                tempQuestion = myQuestions.get(randomQuestion + myMultiAmt + myTFAmt);
+            case 4:
+                randomQuestion = myRand.nextInt(myQuestions.size());
+                tempQuestion = myQuestions.get(randomQuestion);
+            default:
+                tempQuestion = null;
+        }
+
+        return tempQuestion;
     }
-    //need a method to spit out a random question, a random multi question, a random TF question, and one for random short
 }
